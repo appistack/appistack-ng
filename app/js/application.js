95019b9288ea72a535f2c773b2dce1397eae78a2
David Conner <dconner.pro@gmail.com>
2015-07-27 01:48:57 -0400

    removed ngCookies, as nothing depends on it

--- app/js/application.js
+++ app/js/application.js
@@ -1,6 +1,5 @@
 angular.module('app',
     ['ngRoute',
-      'ngCookies',
       'ipCookie',
       'ngResource',
       'restangular',
--- gulpfile.js
+++ gulpfile.js
@@ -109,7 +109,6 @@ var vendorJsFiles = [
   "vendor/bower/underscore.string/lib/underscore.string.js",
   "vendor/bower/angular/angular.js",
   "vendor/bower/angular-cookie/angular-cookie.js",
-  "vendor/bower/angular-cookies/angular-cookies.js",
   "vendor/bower/angular-route/angular-route.js",
   "vendor/bower/angular-resource/angular-resource.js",
   "vendor/bower/angular-bootstrap/ui-bootstrap-tpls.js",
